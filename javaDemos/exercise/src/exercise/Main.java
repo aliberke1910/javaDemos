3b467acb56d929d1ace478e24f6d418ceeea254b
Ali Berke Geçgel <47008493+aliberke1910@users.noreply.github.com>
2022-12-21 22:46:01 +0300

    Add files via upload

---
+++ javaDemos/exercise/src/exercise/Main.java
@@ -0,0 +1,15 @@
+package exercise;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		String[] isim = new String [] {"Ali", "Berke", "Arda"};
+		
+		for (String isimler : isim) {
+			
+			System.out.println(isimler); 
+		}
+		
+	}
+}
